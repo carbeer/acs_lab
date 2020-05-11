c907c32a3322095924f506efedb54b58a3e0179d
carbeer <mail@carolin.beer>
2020-05-11 23:05:49 +0200

    add go code to read first line of given file

---
+++ lab_1_selinux/freader.go
@@ -0,0 +1,25 @@
+package main
+
+import "log"
+import "bufio"
+import "os"
+
+func main() {
+    fname := os.Args[1]
+    log.Print(fname)
+    
+    f, err := os.Open(fname)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer func() {
+        if err = f.Close(); err != nil {
+            log.Fatal(err)
+        }
+    }()
+
+    s := bufio.NewScanner(f)
+    s.Scan()
+    log.Println(s.Text())
+}
