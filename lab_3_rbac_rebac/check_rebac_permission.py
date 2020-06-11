86d0657412e1c36013f795b16dc6bba859fa5376
carbeer <mail@carolin.beer>
2020-06-11 23:10:47 +0200

    add assignment 3

--- lab_3_rbac_rebac/check_rebac_permission.py
+++ lab_3_rbac_rebac/check_rebac_permission.py
@@ -1,35 +1,82 @@
 import sys
+import math
 import json
 import re
 import operator
 import logging
 
-pat = re.compile('h(>|<|=)([0-9])')
+# 0 for assignment 2.1, 1 for assignment 2.2
+version = 0 
+
 ops = {
         "<": operator.le,
         ">": operator.ge,
         "=": operator.eq,
-        }
+    }
+
+def get_neighbors(node: str, usr_graph: dict) -> list:
+    if not node in usr_graph:
+        raise Exception(f"user {node} does not exist")
+    if not isinstance(usr_graph[node], list):
+        raise Exception("expected list as input, check the formatting of your usergraph")
+    return usr_graph[node]
+
+def get_coworkers(node: str, usr_graph: dict) -> list: 
+    if not node in usr_graph:
+        raise Exception(f"user {node} does not exist")   
+    if usr_graph[node] is None:
+        return []
+    if not isinstance(usr_graph[node], dict):
+        raise Exception(f"expected dict as input, check the formatting of your usergraph, got {type(usr_graph[node])}: {usr_graph[node]}")
+    ret = (usr_graph[node]["coworkers"] if "coworkers" in usr_graph[node] else [])
+    return ret
+
+def get_friends(node: str, usr_graph: dict) -> list:
+    if not node in usr_graph:
+        raise Exception(f"user {node} does not exist")
+    if usr_graph[node] is None:
+        return []
+    if not isinstance(usr_graph[node], dict):
+        raise Exception(f"expected dict as input, check the formatting of your usergraph, got {type(usr_graph[node])}: {usr_graph[node]}")
+    ret = (usr_graph[node]["friends"] if "friends" in usr_graph[node] else [])
+    return ret 
+
+
+pat = [
+        [
+            (re.compile('h(>|<|=)([0-9])'), get_neighbors),
+        ],
+        [
+            (re.compile('f(>|<|=)([0-9])'), get_friends),
+            (re.compile('c(>|<|=)([0-9])'), get_coworkers),
+        ],
+    ]
+
+config = [
+        "./rebac.json",
+        "./rebac_extended.json",
+    ]
 
 
 # Returns the policies with the corresponding user as a list of tuples of structure (policy, user)
 def get_policy_per_user(usr: str, res: str, perm: dict) -> list:
-    if not usr in perm["policies"]:
-        return [(None, None)]
     
-    trp, tup = perm["policies"][usr]["trp"], perm["policies"][usr]["tup"]
     ctrl, targets = (None, []) 
     for r in perm["resources"]:
         if r["name"] == res:
             ctrl, targets = r["controller"], r["target"]
             break
+    if ctrl is None:
+        raise Exception("recource does not exist")
+
+    trp, tup = perm["policies"][ctrl]["trp"], perm["policies"][ctrl]["tup"]
     pol_user = list(zip([tup] * len(targets), targets))
     pol_user.append((trp, ctrl))
     for el in pol_user: logging.debug(el) 
     return pol_user 
 
 # Returns the shortest path from source to sink using BFS
-def shortest_path(source: str, sink: str, usrs: list,  usr_graph: dict) -> int:
+def shortest_path(source: str, sink: str, cb: callable, usrs: list, usr_graph: dict) -> int:
     if source == sink:
         logging.debug("User is the target")
         return 0
@@ -40,37 +87,46 @@ def shortest_path(source: str, sink: str, usrs: list,  usr_graph: dict) -> int:
     for i in range(1, len(usrs)):
         next_nodes = [] 
         for node in curr_nodes:
-            if sink in usr_graph[node]:
+            if sink in cb(node, usr_graph):
                 logging.debug(f"Shortest path to {sink} is {i}")
                 return i
-            for neighbor in usr_graph[node]:
-                if not visited[neighbor]:
-                    next_nodes.append(neighbor)
-                    visited[neighbor] = True
+            for el in cb(node, usr_graph):
+                if not visited[el]:
+                    next_nodes.append(el)
+                    visited[el] = True
         curr_nodes = next_nodes
-    return -1
+    logging.info(f"no path between {source} and {sink} using {cb.__name__}")
+    return math.inf 
 
-# Evaluates policy pol given a distance dist
-def evaluate_policy(pol: str, dist: int) -> bool:   
-    if dist == -1:
-        return False
-    matches = pat.match(pol)
+# Evaluates an (atomic) part of a single policy 
+def eval_partial_policy(usr: str, tgt: str, limit: int, op: callable, cb: callable, perm: dict) -> bool:   
+    d =  shortest_path(usr, tgt, cb, perm["users"], perm["usergraph"])
     # Evaluates whether distance *operator (</>/=)* *value defined by policy*
-    return (ops[matches.group(1)](dist, int(matches.group(2))))
+    logging.debug(f"partial policy for target {tgt} with limit {limit} evaluated to {op(d,limit)}")
+    return op(d, limit)
 
-# Evaluates whether usr should have access to res
+# Evaluates a single policy. A single policy is only True is all of its atomic parts are True.
+def eval_policy(pol: str, usr: str, tgt: str, perm: dict) -> bool:
+    parts = pol.split(" & ")
+    for pt in parts: 
+        evaled = False
+        for p, cb in pat[version]:
+            m = p.match(pt)
+            if m and len(m.groups()) == 2:
+                evaled = True
+                op, limit = m.groups() 
+                if not eval_partial_policy(usr, tgt, int(limit), ops[op], cb, perm):
+                    return False
+        if not evaled:
+            raise Exception(f"invalid policy formatting: {pt}")
+    logging.debug(f"policy {pol} evaluated to True")
+    return True
+
+# Evaluates whether usr should have access to res. Uses op to resolve conflicting policy decisions.
 def has_permission(usr: str, res: str, op: str, perm: dict) -> bool: 
-    # all
-    break_cond = False 
-    # any
-    if op == "ANY":
-         break_cond = True 
-    pol_user = get_policy_per_user(usr, res, perm)
-
-    for p in pol_user:
-        if p[0] is None or p[1] is None:
-            return False
-        if evaluate_policy(p[0], shortest_path(usr, p[1], perm["users"], perm["usergraph"])) == break_cond:
+    break_cond = (True if op == "ANY" else False) 
+    for pol, tgt in get_policy_per_user(usr, res, perm):
+        if eval_policy(pol, usr, tgt, perm) == break_cond:
             return break_cond
     return not break_cond 
 
@@ -82,10 +138,10 @@ def main():
     op = sys.argv[3].upper()
     assert op in ["ALL", "ANY"]
 
-    with open("./rebac.json") as f:
-        perms = json.load(f)
-        dec = has_permission(usr, res, op, perms)
-        print("*****Decision*****")
+    with open(config[version]) as f:
+        perm = json.load(f)
+        dec = has_permission(usr, res, op, perm)
+        print("*****Access Decision*****")
         print(dec)
 
 if __name__ == "__main__":
